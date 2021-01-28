e02f4b7f149c70f42964bebeeca13ca252078795
jwd2488 <jwd2488@rit.edu>
2021-01-27 19:31:50 -0500

    item

--- Assets/item.cs
+++ Assets/item.cs
@@ -1,12 +1,23 @@
+using UnityEngine;
+
 namespace DefaultNamespace
 {
-    public class item
+    public class item : MonoBehaviour
     {
         private string name;
         private string description;
-        public item()
+        private SpriteRenderer spr = new SpriteRenderer();
+        public item(string newName, string newDescription)
         {
-            
+            name = newName;
+            description = newDescription;
+        }
+
+        public void del()
+        {
+            name = null;
+            description = null;
+            spr.sprite = null;
         }
     }
 }
